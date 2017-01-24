1ae601e67462f6728a8c62b157c35d016185c5d2
izabelka <i.konstanciak@gmail.com>
2017-01-24 22:34:15 +0100

    Add working Wikipedia api call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import './App.css';
 
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+  		entry: "Te"
+    };
+	}
+
+  getEntries() {
+
+    return $.getJSON('https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=10&origin=*&search=Te')
+      .then((data) => {
+      	console.log(data)
+      });
+  }
   render() {
     return (
       <div className="container">
-      <a target="_blank" href="https://en.wikipedia.org/wiki/Special:Random">Random Wikipedia Article</a>
+      	<a target="_blank" href="https://en.wikipedia.org/wiki/Special:Random">Random Wikipedia Article</a>
       </div>
     );
+  }
+
+  componentDidMount() {
+  	this.getEntries();
   }
 }
 
