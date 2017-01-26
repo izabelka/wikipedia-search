0cf2ad339627b5749379aa7680b8fbd8a7648f89
izabelka <i.konstanciak@gmail.com>
2017-01-26 14:27:46 +0100

    Add component for displaying results

---
+++ src/Result.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Result extends Component {
+
+  render() {
+    return (
+      <div className="result-container">
+        <h3 className="title">
+          {this.props.result.title}
+        </h3>
+        <p className="description">
+          {this.props.result.description}
+        </p>
+        <a className="description" href={this.props.result.link}>
+          {this.props.result.link}
+        </a>
+      </div>
+    )
+  }
+}
+
+export default Result;
